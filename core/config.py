d70550d91e714297b33b0c5bfadfca239639bccb
Akshay Okali <ak20536578@L-156308400.local>
2024-11-05 01:31:03 +0530

    Update core/cofig.py file

--- core/config.py
+++ core/config.py
@@ -3,7 +3,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Transaction Fraud Detection API"
-    database_url: str = "sqlite:///./test.db"
 
     class Config:
         env_file = ".env"
